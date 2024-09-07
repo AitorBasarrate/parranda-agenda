c84899478bbf03ff63f480fdbbd577804f437e99
Aitor Basarrate <basarrate.aitor@gmail.com>
2024-09-07 12:26:18 +0200

    Firebase configured

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,20 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '@envs/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    provideFirestore(() => getFirestore())
+  ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase: {
+        "projectId":"laudioko-jaiak",
+        "appId":"1:565485651695:web:fd94b389736cd819babd1a",
+        "databaseURL":"https://laudioko-jaiak-default-rtdb.europe-west1.firebasedatabase.app",
+        "storageBucket":"laudioko-jaiak.appspot.com",
+        "apiKey":"AIzaSyAbVyYayGUsIfuG8UL2pOZxBmHcA53jYbI",
+        "authDomain":"laudioko-jaiak.firebaseapp.com",
+        "messagingSenderId":"565485651695",
+        "measurementId":"G-Z065Q60PHY"
+      }
+};
